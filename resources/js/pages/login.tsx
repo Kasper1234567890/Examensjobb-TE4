41d000269e6097378ae27e8c71100568bc00adec
Kasper <kasper.bieniek@elev.ga.ntig.se>
2026-03-04 12:48:06 +0100

    First

---
+++ resources/js/pages/login.tsx
@@ -0,0 +1,45 @@
+import { Head, Link, usePage } from "@inertiajs/react";
+import { dashboard, /*login,*/ register } from "@/routes";
+import Logo from "../../assets/Speed_Bricks_Logo.png";
+
+export default function Login({ canRegister = true }: { canRegister?: boolean }) {
+	const { auth } = usePage().props;
+
+	return (
+		<>
+			<Head title="Home">
+				<link rel="preconnect" href="https://fonts.bunny.net" />
+				<link
+					href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<nav className="fixed top-0 z-1000 w-full bg-[#3f3f3f] p-5">
+				<div className="flex justify-between">
+					<div className="flex justify-center gap-4">
+						<a href="/" className="navBtn">
+							Home
+						</a>
+						<a href="/sets" className="navBtn">
+							Sets
+						</a>
+					</div>
+					<div className="flex items-center justify-center">
+						<img src={Logo} alt="Speed Bricks Logo" className="w-1/4" />
+					</div>
+					<div className="flex justify-center gap-4">
+						<a href="/signup" className="navBtn">
+							Sign Up
+						</a>
+						<a href="/login" className="navBtn">
+							Log In
+						</a>
+					</div>
+				</div>
+			</nav>
+
+			<footer></footer>
+		</>
+	);
+}
